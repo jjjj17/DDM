fixed dt use in dx formula, added drft rate calculation and changed time dim for plot to ms

ddm_model.py
@@ -9,10 +9,11 @@ import plotly.express as px # type: ignore
 
 class DDM():
     def __init__(self, dt, input_a, input_b, noise_mag, threshold, initial_condition = 0):
-        self.dt = dt
+        self.dt = dt #in ms
         self.input_a = input_a
         self.input_b = input_b
-        self.noise_mag = noise_mag
+        self.drift_rate = self.input_a - self.input_b
+        self.noise_mag = noise_mag #noise values should go from 0 to 1.
         self.threshold = threshold
         self.initial_condition = initial_condition
 
@@ -26,9 +27,9 @@ class DDM():
             x = self.initial_condition
             traj = [x]
             for y in range(timesteps):
-                noise_term = np.random.randn()
-                dxdt = (self.input_a - self.input_b) + (self.noise_mag * noise_term)
-                x += dxdt
+                noise_term = np.random.randn() * np.sqrt(self.dt)
+                dx = ((self.drift_rate)*self.dt) + (self.noise_mag * noise_term)
+                x += dx
                 traj.append(x)
 
                 if x >= self.threshold:
@@ -46,7 +47,8 @@ class DDM():
     
     def plot_trajectories(self):
         s = pd.DataFrame(self.simulated_trajectories).T
-        fig = px.line(s, title="Drift-Diffusion Model Simulations", labels={"index": "Time Step", "value": "Decision Variable x(t)"})
+        s.index = s.index * self.dt
+        fig = px.line(s, title="Drift-Diffusion Model Simulations", labels={"index": "Time (ms)", "value": "Decision Variable x(t)"})
         fig.add_hline(y=self.threshold, line_dash="dash", line_color="red", annotation_text="Decision A")
         fig.add_hline(y=-self.threshold, line_dash="dash", line_color="blue", annotation_text="Decision B")
         fig.show()